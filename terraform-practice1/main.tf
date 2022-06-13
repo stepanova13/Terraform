46d78a71e98bda2d428d3d78de1a90e6f6da240c
alex <alexandra.stepanova10@gmail.com>
2022-06-13 17:27:03 -0400

    Initial commit

---
+++ terraform-practice1/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region  = "us-west-2"
+  profile = "tf-cli-west2"
+}
+
+# Deploy an EC2 instance using Amazon Linux 2 in default VPC in availability zone us-west-2b.
+resource "aws_instance" "amazon_linux_2_server" {
+  ami                  = "ami-0ca285d4c2cda3300"
+  instance_type        = "t2.micro"
+  availability_zone    = "us-west-2b"
+  key_name             = "oregon-key-pair"
+  iam_instance_profile = "ec2-to-ssm-full-access"
+  tags = {
+    "Name" = ":) Hello Terraform World"
+  }
+}
+
+
+# Create a VPC resource 10.123.0.0/16 in us-west-2.
+resource "aws_vpc" "oregon_vpc" {
+  cidr_block = "10.123.0.0/16"
+  tags = {
+    "Name" = "Terraform VPC"
+  }
+}
+
