ab1ef7e25b245afd431929cb54e0ad345582e0ed
alex <alexandra.stepanova10@gmail.com>
2022-06-21 16:25:34 -0400

    create folder terraform-practice2 + files

--- terraform-practice2/main.tf
+++ terraform-practice2/main.tf
@@ -16,9 +16,7 @@ data "aws_region" "current" {}
 # Deploy an EC2 instance using Amazon Linux 2 in default VPC 
 resource "aws_instance" "amazon_linux_2_server" {
   ami = data.aws_ami.amazon_linux_2_ami.id # Reference the data source "aws_ami" attribute - id
-  #  instance_type          = var.instance_type                  # Reference the value of the variable specified
   instance_type = var.ec2_types["dev"] # reference a map
-
   availability_zone      = var.availability_zone
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.sg_rds_ssh_http.id]
