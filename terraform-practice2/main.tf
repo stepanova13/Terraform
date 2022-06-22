55939fa5b04145e6af67ec702b4b62f6410d5f80
alex <alexandra.stepanova10@gmail.com>
2022-06-22 00:38:15 -0400

    change

--- terraform-practice2/main.tf
+++ terraform-practice2/main.tf
@@ -12,18 +12,23 @@ data "aws_ami" "amazon_linux_2_ami" {
 
 data "aws_region" "current" {}
 
+data "aws_availability_zone" "oregon-b" {
+  name = "us-west-2b"
+}
+
 # RESOURCES
 # Deploy an EC2 instance using Amazon Linux 2 in default VPC 
 resource "aws_instance" "amazon_linux_2_server" {
   ami = data.aws_ami.amazon_linux_2_ami.id # Reference the data source "aws_ami" attribute - id
   #  instance_type          = var.instance_type                  # Reference the value of the variable specified
-  instance_type          = var.ec2_types["dev"] # reference a map
-  availability_zone      = var.availability_zone
+  instance_type = var.ec2_types["dev"] # reference a map
+  # availability_zone      = var.availability_zone
+  availability_zone      = data.aws_availability_zone.oregon-b.name
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.sg_rds_ssh_http.id]
   tags = {
     (var.ec2_tags[0]) = data.aws_region.current.name
-    (var.ec2_tags[1]) = var.availability_zone
+    (var.ec2_tags[1]) = data.aws_availability_zone.oregon-b.name
   }
 }
 
